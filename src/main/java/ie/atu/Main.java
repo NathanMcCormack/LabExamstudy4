1a10d020fbafb11fcaab9305c1421bb926a9f8f0
NathanMcCormack <G00414780@atu.ie>
2023-12-10 18:53:44 +0000

    Program asks user to input 10 integer, inputs are caught ad checked

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,27 @@
 package ie.atu;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> integerList = new ArrayList<>();
+
+        try {
+            System.out.println("Enter 10 integers, one at a time:");
+
+            for (int i = 0; i < 10; i++) {
+                System.out.print("Enter integer #" + (i + 1) + ": ");
+                int input = scanner.nextInt();
+                integerList.add(input);
+            }
+
+            System.out.println("Entered integers: " + integerList);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter a valid integer.");
+        }
+
     }
 }
